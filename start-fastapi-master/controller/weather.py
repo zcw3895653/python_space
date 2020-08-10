b1222b4debc08d6b3d24bba170375f5ab05254a8
scott_zhou <zcw3895653@126.com>
2020-08-10 20:57:03 +0800

    new

--- start-fastapi-master/controller/weather.py
+++ start-fastapi-master/controller/weather.py
@@ -9,10 +9,33 @@ LOGGER = get_controller_logger('TIANQI')
 
 
 @router.get('/weather/{city_id}')
-def get_tianqi(city_id: str):
-    LOGGER.info('Get tianqi with id: %s' % city_id)
-    tianqi = tianqi_service.get_tianqi(city_id)
-    if not tianqi:
-        return error(msg='Cannot found item with id %s' % city_id)
-    return success(tianqi)
+async def get_tianqi(city_id: str):
+            LOGGER.info('Get tianqi with id: %s' % city_id)
+            tianqi = tianqi_service.get_tianqi(city_id)
+            if not tianqi:
+                return error(msg='Cannot found item with id %s' % city_id)
+            return success(tianqi)
 
+@router.post('/weather/')
+async def get_tianqi(city_id: str):
+            LOGGER.info('Get tianqi with id: %s' % city_id)
+            tianqi = tianqi_service.get_tianqi(city_id)
+            if not tianqi:
+                return error(msg='Cannot found item with id %s' % city_id)
+            return success(tianqi)
+
+@router.post('/weather/forecast')
+async def get_tianqi(city_id: str):
+            LOGGER.info('Get tianqi with id: %s' % city_id)
+            tianqi = tianqi_service.get_tianqi(city_id)
+            if not tianqi:
+                return error(msg='Cannot found item with id %s' % city_id)
+            return success(tianqi)
+
+@router.post('/air/now')
+async def get_tianqi(city_id: str):
+            LOGGER.info('Get tianqi with id: %s' % city_id)
+            tianqi = tianqi_service.get_tianqi(city_id)
+            if not tianqi:
+                return error(msg='Cannot found item with id %s' % city_id)
+            return success(tianqi)
--- start-fastapi-master/service/tianqi.py
+++ start-fastapi-master/service/tianqi.py
@@ -12,23 +12,23 @@ LOGGER = get_service_logger('Tianqi')
 def get_tianqi(cityid: str) -> Tianqi:
     day =datetime.datetime.now().strftime('%Y-%m-%d')
     key=day+"-"+cityid
-    sql="select * from tianqi where tianqi_id='%(name)s'" % {'name':key}
+    sql="select city,cityid,temp from tianqi where tianqi_id='%(name)s'" % {'name':key}
     res={}
     LOGGER.info("finding tian with key="+key)
     db_data=database.query(sql)
-    if len(db_data)==0:
-        api_data=weather_http.get_api("http://www.weather.com.cn/data/sk/%s.html" %cityid)
+    if db_data!=None:
+        res['city'] = db_data[0]
+        res['cityid'] = db_data[1]
+        res['temp'] = db_data[2]
+        res['tianqi_id'] = key
+    else:
+        api_data = weather_http.get_api("http://www.weather.com.cn/data/sk/%s.html" % cityid)
         print(api_data)
-        res['city']=api_data['weatherinfo']['city']
+        res['city'] = api_data['weatherinfo']['city']
         res['cityid'] = api_data['weatherinfo']['cityid']
         res['temp'] = api_data['weatherinfo']['temp']
         res['tianqi_id'] = key
         update_tianqi(res)
-    else:
-        res['city'] = db_data['city']
-        res['cityid'] =db_data['cityid']
-        res['temp'] = db_data['temp']
-        res['tianqi_id'] = key
     print(res)
     return res
 
